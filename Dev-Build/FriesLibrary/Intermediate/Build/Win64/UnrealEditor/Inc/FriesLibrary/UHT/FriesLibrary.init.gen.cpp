11145ab513fb3f16b5bcc99ff3b17eb16d80406e
Frytas <107775728+Frytasplayer@users.noreply.github.com>
2023-12-06 11:09:00 -0300

    Build to Unreal 5.3

--- Dev-Build/FriesLibrary/Intermediate/Build/Win64/UnrealEditor/Inc/FriesLibrary/UHT/FriesLibrary.init.gen.cpp
+++ Dev-Build/FriesLibrary/Intermediate/Build/Win64/UnrealEditor/Inc/FriesLibrary/UHT/FriesLibrary.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeFriesLibrary_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x674682CC,
+				0x9F28AD1A,
 				0x40E7CFB6,
-				METADATA_PARAMS(nullptr, 0)
+				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_FriesLibrary.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_FriesLibrary.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FriesLibrary(Z_Construct_UPackage__Script_FriesLibrary, TEXT("/Script/FriesLibrary"), Z_Registration_Info_UPackage__Script_FriesLibrary, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x674682CC, 0x40E7CFB6));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FriesLibrary(Z_Construct_UPackage__Script_FriesLibrary, TEXT("/Script/FriesLibrary"), Z_Registration_Info_UPackage__Script_FriesLibrary, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9F28AD1A, 0x40E7CFB6));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
